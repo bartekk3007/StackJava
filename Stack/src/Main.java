a310d12223b612e4530d0b8beaf47de5e42badd7
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-12-20 23:54:07 +0100

    Add files via upload

---
+++ Stack/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        var stack = new Stack<String>();
+        stack.push("Andrzej");
+        stack.push("Bartek");
+        stack.push("Cezary");
+        System.out.println(stack);
+    }
+}
